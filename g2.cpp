10c3e9aa7472eefcafe4706fe3c24e7c82eec835
Shikha Kumari <112888553+09shikhakumari@users.noreply.github.com>
2026-06-22 10:35:43 +0530

    Add files via upload

---
+++ g2.cpp
@@ -0,0 +1,12 @@
+//Write a Program to Read and Print Number Input from the User.
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "You entered: " << num << endl;
+    return 0;
+}
---
+++ g3.cpp
@@ -0,0 +1,12 @@
+//Write a Program to Print the ASCII Value of a Character.
+#include <iostream>
+using namespace std;
+
+// Driver code
+int main()
+{
+    char c = 'A';
+    cout << "The ASCII value of " << 
+             c << " is " << int(c);
+    return 0;
+}
